f44563f5668b7ea015fee2273f59927ff00a7808
deathlbc <95583092+deathlbc@users.noreply.github.com>
2022-02-12 11:39:24 +0800

    Update testTogether.py

--- testTogether.py
+++ testTogether.py
@@ -9,12 +9,7 @@ def ssubtraction_f(a,b):
 
 def mul(x, y):
     return x * y
-7 lines (7 sloc)  139 Bytes
    
-def addf():
-    a = int(input("請輸入被加數:"))
-    b = int(input("請輸入加數:"))
-    c = a + b
-    print(c)
-    return c
+def addf(a,b):
+    return a + b
 addf()

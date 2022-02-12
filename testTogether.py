6754d41115c87002c2a050d068005420e214794f
Yulissa-wu <97188330+Yulissa-wu@users.noreply.github.com>
2022-02-13 06:10:29 +0800

    Update testTogether.py

--- testTogether.py
+++ testTogether.py
@@ -1,20 +1,8 @@
-def division(x, y):
-    return x / y
-
-def square(a,b):
-    return a**b
-
-def ssubtraction_f(a,b):
-    return a-b
-
-def mul(x, y):
-    return x * y
-7 lines (7 sloc)  139 Bytes
-   
 def addf():
     a = int(input("請輸入被加數:"))
     b = int(input("請輸入加數:"))
-    c = a + b
+    c = "這是您的和:", a + b
+    c = ''.join(str(i) for i in c)
     print(c)
     return c
 addf()
